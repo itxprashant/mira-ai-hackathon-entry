e49ef1c104d7c38698a36cfef9a1a18f4f4e98e9
Prashant <prashantt492@gmail.com>
2025-02-08 16:18:24 +0530

    Created a minimalistic frontend

--- main.py
+++ main.py
@@ -1,18 +1,44 @@
 from mira_sdk import MiraClient, Flow
 
-# Initialize the client
-client = MiraClient(config={"API_KEY": "sb-6113a5cb58f6b3c5660b07363f88b025"})
+genre_list = ["Action", "Adventure", "Comedy", "Crime and Mystery", "Fantasy", "Historical",
+              "Horror", "Romance", "Satire", "Sci-fi", "Speculative", "Thriller", "Others"]
 
-version = "1.0.0"
-input_data = {
-"user-prompt": "A romantic sci-fi"
-}
+language_list = ["English", "Hindi", "Others"]
+def return_response(prompt, genre_list, mood):
+    # Initialize the client
+    client = MiraClient(config={"API_KEY": "sb-6113a5cb58f6b3c5660b07363f88b025"})
 
-# If no version is provided, latest version is used by default
-if version:
-    flow_name = f"@itxprashant/movie-book-recommender/{version}"
-else:
-    flow_name = "@itxprashant/movie-book-recommender"
+    version = "1.0.0"
+    input_data = {
+    "user-prompt": f"""{prompt}, List of Genres: {genre_list}, 
+    First show the movie/books/series which contains all the given genre, then show them seperately
+    
+    My mood is {mood}"""
+    }
 
-result = client.flow.execute(flow_name, input_data)
-print(result)
+    # If no version is provided, latest version is used by default
+    if version:
+        flow_name = f"@itxprashant/movie-book-recommender/{version}"
+    else:
+        flow_name = "@itxprashant/movie-book-recommender"
+
+    result = client.flow.execute(flow_name, input_data)
+    return result
+
+def return_response_based_on_watched(prompt):
+    client = MiraClient(config={"API_KEY": "sb-6113a5cb58f6b3c5660b07363f88b025"})
+
+    version = "1.0.0"
+    input_data = {
+    "user-prompt": f""" 
+    I've watched these movies, recommend me something to my flavour: 
+    {prompt}"""
+    }
+
+    if version:
+        flow_name = f"@itxprashant/movie-book-recommender/{version}"
+    else:
+        flow_name = "@itxprashant/movie-book-recommender"
+
+    result = client.flow.execute(flow_name, input_data)
+    return result
